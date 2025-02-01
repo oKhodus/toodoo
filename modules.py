291690602b3b15d7885506b0ab8603548a336049
Oleksii Khodus <exalchel@gmail.com>
2025-02-01 21:47:58 +0200

    added modules [add, rm, sls]

---
+++ modules.py
@@ -0,0 +1,37 @@
+import json
+
+def add_task(task):
+
+    data = task
+    
+    try:
+        with open("data.json", "r") as file:
+            tasks = json.load(file)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
+    tasks.append(data)
+
+    with open("data.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+    return f"Task was added in data.json"
+
+def rm_task(task):
+    try:
+        with open("data.json", "r") as file:
+            tasks = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
+
+    tasks.remove(task)
+
+    with open("data.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+    return f"Task was remove from data.json"
+
+def sls():
+    with open("data.json", "r") as file:
+        tasks = "\n".join(json.load(file))
+        return f"List of tasks you need to do:\n\n{tasks}".upper()
--- todo.py
+++ todo.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
-import sys
+import sys, json
+
+from modules import *
 
 def new():
-    todo = sys.argv[0]
-    correct_cmds = ["add", "rm", "upd", "sls",]
-    cmd = input("Enter your cmd: ")
-    print(f"your cmd in {todo} was {cmd}")
+    todo_cmd = sys.argv[1]
+    if len(sys.argv) > 1:
+        todo_task = " ".join(sys.argv[2:])
+
+    correct_cmds = ["add", "rm", "upd", "sls"]
+
+    if todo_cmd in correct_cmds:
+        print(f"your cmd was [{todo_cmd}]../../\n")
+        if todo_cmd == "add": print(add_task(todo_task))
+        if todo_cmd == "rm": print(rm_task(todo_task))
+        if todo_cmd == "sls": print(sls())
+        
+    
 new()
