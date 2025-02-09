782dbcaf46213ca33691a9b20092c58d039d32cf
Oleksii Khodus <exalchel@gmail.com>
2025-02-09 23:46:37 +0200

    added [-help] cmd to modules

--- modules.py
+++ modules.py
@@ -1,21 +1,20 @@
 import json
 
-def add_task(task):
 
-    data = task
-    
+def add_task(task):
     try:
         with open("data.json", "r") as file:
             tasks = json.load(file)
 
     except (FileNotFoundError, json.JSONDecodeError):
         tasks = []
-    tasks.append(data)
+    tasks.append(task)
 
     with open("data.json", "w") as file:
         json.dump(tasks, file, indent=4)
 
     return f"Task was added in data.json"
+
 
 def rm_task(task):
     try:
@@ -31,7 +30,13 @@ def rm_task(task):
 
     return f"Task was remove from data.json"
 
+
 def sls():
     with open("data.json", "r") as file:
         tasks = "\n".join(json.load(file))
         return f"List of tasks you need to do:\n\n{tasks}".upper()
+
+
+def help():
+    with open("help_doc.txt", "r") as file:
+        return file.read()
--- todo.py
+++ todo.py
@@ -3,18 +3,29 @@ import sys, json
 
 from modules import *
 
-def new():
+
+def main():
     todo_cmd = sys.argv[1]
     if len(sys.argv) > 1:
         todo_task = " ".join(sys.argv[2:])
 
-    correct_cmds = ["add", "rm", "upd", "sls"]
+    correct_cmds = ["-help", "add", "rm", "upd", "sls", "sls/d", "sls/p"]
 
     if todo_cmd in correct_cmds:
         print(f"your cmd was [{todo_cmd}]../../\n")
-        if todo_cmd == "add": print(add_task(todo_task))
-        if todo_cmd == "rm": print(rm_task(todo_task))
-        if todo_cmd == "sls": print(sls())
-        
-    
-new()
+        if todo_cmd == "add":
+            print(add_task(todo_task))
+        elif todo_cmd == "rm":
+            print(rm_task(todo_task))
+        elif todo_cmd == "sls":
+            print(sls())
+        elif todo_cmd == "-help":
+            print(help())
+    else:
+        print(
+            "Your command doesn't exists.\nType [-help] to check list of correct commands."
+        )
+
+
+if __name__ == "__main__":
+    main()
